b75083c6e0a87a22f532f335cc59a7463d3b698e
Ali Abdukarim <alisher.coder@gmail.com>
2024-05-17 08:47:04 -0500

    project and app created

---
+++ tasks_interview/tasks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# Create Task model
+# task model should have title, description, is_complete fields
+# Model should have title as string representation
+ 
